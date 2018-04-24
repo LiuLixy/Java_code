b64a61a4fb9d7ba0d98e104e2d7f1de7ed21102f
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-04-24 19:53:30 +0800

    多线程
    使用Runnable接口创建三个子线程并起名为A，B，C模拟实现卖票操作，观察结果。

---
+++ Test1.java
@@ -0,0 +1,23 @@
+class MyThread implements Runnable {
+	private int ticket = 10;
+	private String title;
+	public MyThread(String title) {
+		this.title = title;
+	}
+	public void run() {
+		while(this.ticket > 0) {
+			System.out.println(this.title+": "+"Ê£ÓàÆ±Êı: "+this.ticket--);
+		}
+	}
+}
+
+public class Test1 {
+	public static void main(String[] args) {
+		MyThread myThreadA = new MyThread("A");
+		MyThread myThreadB = new MyThread("B");
+		MyThread myThreadC = new MyThread("C");
+		new Thread(myThreadA).start();
+		new Thread(myThreadB).start();
+		new Thread(myThreadC).start();
+	}
+}
