c71f94ca6ba62b30a1961adec726a70380400ed2
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-05-22 10:57:28 +0800

    判断一个数是否为回文数

--- 回文数.java
+++ 回文数.java
@@ -5,23 +5,25 @@ import java.util.*;
 
 public class Test {
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.print("请输入一个5位数: ");
 		while(sc.hasNext()) {
 			int num = sc.nextInt();
-			if (num / 10000 > 0 && num / 10000 < 10) {
-				if((num/10000 == num % 10) && 
-						(num/1000%10 == num % 100 / 10)) {
-					System.out.println(num+" 是回文数..");
-					break;
-				} else {
-					System.out.println(num+" 不是回文数..");
-					break;
-				}
+			HuiWen(num);
+			break;
+		}
+	}
+	public static void HuiWen(int num) {
+		if ((num / 10000 > 0) && (num / 10000 < 10)) {
+			if((num/10000 == num % 10) && 
+					(num/1000%10 == num % 100 / 10)) {
+				System.out.println(num+"是回文数..");
 			} else {
-				System.out.println("输入的数不是一个5位数..");
-				break;
+				System.out.println(num+"不是回文数..");
 			}
+		} else {
+			System.out.println("输入的数不是一个5位数..");
 		}
 	}
 }
