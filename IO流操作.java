1a33eb3092bf650288a5c6407a1f3a539dffd283
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-06-29 19:50:19 +0800

    IO流操作

---
+++ IO流操作.java
@@ -0,0 +1,43 @@
+要求:在桌面上新建一个Test.txt,使用打印流向文件中输出如下: 
+Hello world 
+hello Java
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class TestIO {
+	public static void main(String[] args) throws Exception {
+		File file = new File("C:\\Users\\LiuWang\\Desktop\\Test.txt");
+		// OutputStream output = new FileOutputStream(file);
+		// String str = "Hello world\r\nhello Java"; 
+		// output.write(str.getBytes());
+		// output.close();
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		PrintStream printStream = new PrintStream(new FileOutputStream(file));
+		printStream.print("Hello ");
+		printStream.println("world");
+		printStream.print("hello Java");
+		printStream.close();
+	}
+}	
+
+
+然后使用Scanner类读取文件内容并输入到控制台。
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class TestIO {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new FileInputStream(new File("C:\\Users\\LiuWang\\Desktop\\Test.txt")));
+		sc.useDelimiter("\r\n");
+		while(sc.hasNext()) {
+			System.out.println(sc.next());
+		}
+		sc.close();
+	}
+}
